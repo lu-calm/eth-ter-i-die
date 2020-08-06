1d2dc75c01cfe6c0f7255e3aaa6f1401762e2474
lu-calm <lucalm@tutanota.com>
2020-08-06 14:28:07 +1000

    UNDO!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
+ReactDOM.render(< App />, document.getElementById('root'));
