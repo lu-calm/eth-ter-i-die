82dbb3dd2f615f82eb1e83833d3a252eebc55728
lu-calm <lucalm@tutanota.com>
2020-08-06 14:04:58 +1000

    Not sure if this works tbh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from ‘react-router-dom’;
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
